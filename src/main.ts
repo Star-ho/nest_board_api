02ea0a20b87d7057e22c95fc7199f6b54acae377
Star-ho <58648988+Star-ho@users.noreply.github.com>
2021-04-01 23:06:43 +0900

    포트수정

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);//포트 설정
+  await app.listen(process.env.nestport||3000);//포트 설정
 }
 
 
